a80c2830d1a4b4c71393965f9874c60b48390c43
sandyp007 <prajapatisan7@gmail.com>
2024-03-14 17:04:45 +0530

    Day21 Solution

---
+++ Day21/Swapping_Nodes_in_a_Linked_List.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/swapping-nodes-in-a-linked-list/submissions/1201334583/
+
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+        ListNode *left_ptr = head, *right_ptr = head;
+        for (int i = 0; i < k-1; i++) {
+            right_ptr = right_ptr->next;
+        }
+        
+        ListNode *end_ptr = right_ptr;
+        while (right_ptr->next) {
+            left_ptr = left_ptr->next;
+            right_ptr = right_ptr->next;
+        }
+        
+        swap(end_ptr->val, left_ptr->val);
+        
+        return head;
+    }
+};
