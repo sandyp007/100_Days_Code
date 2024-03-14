c8d9ee175b0821f0cbfe73d7025af02942b049c3
sandyp007 <prajapatisan7@gmail.com>
2024-03-14 17:03:02 +0530

    Day21 Solution

---
+++ Day21/Remove_Zero_Sum_Consecutive_Nodes_from_Linked_List.cpp
@@ -0,0 +1,39 @@
+//  https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/
+
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        
+        ListNode* root =new ListNode(0);
+        root->next=head;
+        unordered_map<int,ListNode*>um;
+        um[0]=root;
+        int ac=0;
+        
+        while(head!=NULL){
+            ac+=head->val;
+            
+            //found value
+            if(um.find(ac)!=um.end()){
+                ListNode* prev= um[ac];
+                ListNode* start= prev;
+                
+                //delete bad references
+                int aux = ac;
+                while(prev!=head){
+                    prev=prev->next;
+                    aux+=prev->val;
+                    if(prev!=head)um.erase(aux);
+                }
+                
+                start->next = head->next;
+            }else{
+                um[ac]= head;
+            }
+            
+            head=head->next;
+        }
+        
+        return root->next;
+    }
+};
