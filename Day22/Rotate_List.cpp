aae47d20d502072a7173db67af6115da6bab84ef
sandyp007 <prajapatisan7@gmail.com>
2024-03-15 13:26:09 +0530

    Day22 Solution

---
+++ Day22/Rotate_List.cpp
@@ -0,0 +1,54 @@
+//  https://leetcode.com/problems/rotate-list/  
+
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+       
+        ListNode *temp = head, *ptr = head;
+        
+		// if  list is empty return NULL
+        if(head == NULL)
+            return NULL;
+			
+        // if  list contain 1 node return NULL
+        if(head -> next == NULL)
+            return head;
+        
+        int cnt = 1;
+        
+		// counting nodes in list
+        while(temp->next != NULL)
+        {
+            temp = temp -> next;
+            cnt++;
+        }
+		
+		// mod of k with cnt to find the minimum no of rotations.
+        k = k % cnt;
+        
+        while(k--)
+        {
+            while(temp->next != NULL)
+                temp = temp -> next;
+        
+            temp -> next = head;           //making it a circular linked list
+			
+			// making last node the new head
+            head = temp;
+            ptr = temp;
+			
+			// moving list values one by one
+            while(temp -> next != head)
+            {
+                temp = temp -> next;
+            }
+            
+			// adding NULL at end of list
+            temp -> next = NULL;
+			
+            temp = ptr;
+            
+        }
+        return head;
+    }
+};
