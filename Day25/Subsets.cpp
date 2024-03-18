ef6159334591c7aa11f62b6a1cb6a218cb623f1c
sandyp007 <prajapatisan7@gmail.com>
2024-03-18 12:51:00 +0530

    Day25 Solution

---
+++ Day25/Subsets.cpp
@@ -0,0 +1,20 @@
+//  https://leetcode.com/problems/subsets/
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>res;
+        backtrack(nums, 0, vector<int>(), res);
+        return res;
+    }
+    
+    void backtrack(vector<int>& nums, int k, vector<int> subset, vector<vector<int>>& res){
+        if(k == nums.size()){
+            res.push_back(subset);
+            return;
+        }
+        backtrack(nums, k+1, subset, res);
+        subset.push_back(nums[k]);
+        backtrack(nums, k+1, subset, res);
+    }
+};
