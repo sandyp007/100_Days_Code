147e8a02bf7d4cbd75569ecf591cee20d6dc2dca
sandyp007 <prajapatisan7@gmail.com>
2024-03-19 15:01:15 +0530

    Day26 Solution

---
+++ Day26/Climbing_Stairs.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/climbing-stairs/
+
+class Solution {
+public:
+    int solve(int n,vector<int> &dp){
+        //base case
+        if(n<=2)
+          return n;
+        
+        if(dp[n]!=-1) 
+          return dp[n]; 
+        
+        dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        return dp[n];
+    }
+    int climbStairs(int n) {
+        if(n<=2)
+         return n;
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
