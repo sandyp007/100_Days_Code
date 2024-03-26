8f93c4e7bbe5fc797fe2cd91406eca86ada09370
sandyp007 <prajapatisan7@gmail.com>
2024-03-26 16:44:56 +0530

    Day33 Solution

---
+++ Day33/Find_The_Original_Array_of_Prefix_Xor.cpp
@@ -0,0 +1,13 @@
+//  https://leetcode.com/problems/find-the-original-array-of-prefix-xor/
+
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+        vector<int> result(pref.size());
+        result[0] = pref[0];
+        for(int i = 1; i < pref.size(); i++) {
+            result[i] = pref[i] ^ pref[i-1];
+        }
+        return result;
+    }
+};
