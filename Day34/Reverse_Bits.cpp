2f27c8764a0f11069857c515265fb9c1961031fb
sandyp007 <prajapatisan7@gmail.com>
2024-03-27 09:08:57 +0530

    Day34 Solution

---
+++ Day34/Reverse_Bits.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/reverse-bits/
+
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans=0,count=32;
+        //ans<<=1;
+        //n>>=1;
+        while(count){
+            ans+=n&1;
+            count--;
+            n>>=1;
+            if(count) ans<<=1;
+        }
+        return ans;
+    }
+};
