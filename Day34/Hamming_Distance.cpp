c198295fee79ffb4a922377685277a819ce84275
sandyp007 <prajapatisan7@gmail.com>
2024-03-27 09:08:40 +0530

    Day34 Solution

---
+++ Day34/Hamming_Distance.cpp
@@ -0,0 +1,13 @@
+//  https://leetcode.com/problems/hamming-distance/
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int z=x^y;int count=0;
+        while(z){
+            if(z&1)count++;
+            z=z>>1;
+        }
+        return count;
+    }
+};
