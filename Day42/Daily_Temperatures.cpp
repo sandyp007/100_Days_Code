e911084b4e1db368e255a21eef7aaf255d1ab258
sandyp007 <prajapatisan7@gmail.com>
2024-04-04 17:08:31 +0530

    Day42 Solution

---
+++ Day42/Daily_Temperatures.cpp
@@ -0,0 +1,18 @@
+//  https://leetcode.com/problems/daily-temperatures/
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& t) {
+        int n = t.size();
+        vector<int> ans(n, 0);
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && t[i] > t[st.top()]) {
+                ans[st.top()] = i - st.top();
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
