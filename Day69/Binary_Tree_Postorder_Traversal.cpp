cadc803e4228f4be4a5581682bedc1f47a44823e
sandyp007 <prajapatisan7@gmail.com>
2024-05-01 15:37:58 +0530

    Day69 Solution

---
+++ Day69/Binary_Tree_Postorder_Traversal.cpp
@@ -0,0 +1,27 @@
+//  https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>ans;
+    vector<int> postorderTraversal(TreeNode* root) {
+      
+        if(root != NULL){
+            postorderTraversal(root -> left);
+            postorderTraversal(root -> right);
+            ans.push_back(root -> val);
+           
+        }
+         return ans;
+    }
+};
