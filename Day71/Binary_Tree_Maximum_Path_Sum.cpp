154a6889934fecf9ee209770d87d064edd53284d
sandyp007 <prajapatisan7@gmail.com>
2024-05-03 14:54:54 +0530

    Day71 Solution

---
+++ Day71/Binary_Tree_Maximum_Path_Sum.cpp
@@ -0,0 +1,29 @@
+//  https://leetcode.com/problems/binary-tree-maximum-path-sum/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        int maxSum = INT_MIN;
+        DFS(root, maxSum);
+        return maxSum;
+    }
+    
+    int DFS(TreeNode* root, int& maxSum){
+        if(!root) return 0;
+        int left = max(0, DFS(root->left, maxSum));
+        int right = max(0, DFS(root->right, maxSum));
+        maxSum = max(maxSum, left + right + root->val);
+        return max(left, right) + root->val;
+    }
+};
