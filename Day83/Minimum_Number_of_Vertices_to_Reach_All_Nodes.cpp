1c4db436fec78dc51253cbada68f1b258a30499f
sandyp007 <prajapatisan7@gmail.com>
2024-05-15 18:45:03 +0530

    Day83 Solution

---
+++ Day83/Minimum_Number_of_Vertices_to_Reach_All_Nodes.cpp
@@ -0,0 +1,18 @@
+//  https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/
+
+class Solution {
+public:
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<int> indegree(n, 0);
+        for(auto edge : edges)
+            indegree[edge[1]]++;
+
+        vector<int> ans;
+        for(int i = 0;i < n; i++){
+            if(indegree[i]==0)
+                ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
