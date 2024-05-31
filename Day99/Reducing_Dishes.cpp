80865aa44bef2e86ab0af7ffbad8e5217289b5c5
sandyp007 <prajapatisan7@gmail.com>
2024-05-31 17:46:58 +0530

    Day99 Solution

---
+++ Day99/Reducing_Dishes.cpp
@@ -0,0 +1,18 @@
+//  https://leetcode.com/problems/reducing-dishes/
+
+class Solution {
+public:
+    int maxSatisfaction(vector<int>& satisfaction) {
+        sort(satisfaction.begin(), satisfaction.end(), greater<int>());
+        int n = satisfaction.size();
+        int presum = 0, res = 0;
+        for (int i = 0; i < n; i++) {
+            presum += satisfaction[i];
+            if (presum < 0) {
+                break;
+            }
+            res += presum;
+        }
+        return res;
+    }
+};
